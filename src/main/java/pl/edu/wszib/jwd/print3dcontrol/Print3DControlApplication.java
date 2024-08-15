73593b58e37be5f9fe5611ea98e798570a831c36
Micha Domaradzki <michalunoo@gmail.com>
2024-08-15 15:53:08 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/jwd/print3dcontrol/Print3DControlApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.print3dcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Print3DControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Print3DControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/jwd/print3dcontrol/Print3DControlApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.print3dcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Print3DControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
